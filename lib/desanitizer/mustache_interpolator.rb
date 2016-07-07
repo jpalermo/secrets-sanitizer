4f9ff3bbe3070c73cbb3fae220b5f8a8b222b421
Topher Lubaway <clubaway@pivotal.io>
2016-07-06 17:46:05 -0700

    Refactoring interpolation
    - adding red colored text
    
    Signed-off-by: James Young <jayoung@pivotal.io>

--- lib/desanitizer/mustache_interpolator.rb
+++ lib/desanitizer/mustache_interpolator.rb
@@ -11,20 +11,24 @@ module Desanitizer
 
     def interpolate(name, value, hierarchy)
       path = hierarchy.join('_')
-        if (value.nil?)
-          @logger.warn "========Found nil value for key #{name}, skipping..."
-          return
-        end
 
-        if (value.to_s =~ /{{.*}}/) # {{mustache}}
-          @logger.warn "Going to replace a mustache syntax value "
-          focus = @yaml
-          (0 .. hierarchy.size - 2).each do |depth|
-            focus = focus.fetch(hierarchy[depth])
-          end
-          focus[hierarchy[-1]] = Mustache.render(value, @secrets) #replace with  value of mustache placeholder'
-        end
-     end
+      # this test is of questionable value
+      if (value.nil?)
+        @logger.warn "\e[31m Found nil value for key #{name}, skipping... \e[0m "
+        return
+      end
+
+      return unless (value.to_s =~ /{{.*}}/) # {{mustache}}
+      # there's a mustache value, let's render it
+
+      @logger.warn "\e[31m Going to replace a mustache syntax value \e[0m "
+      focus = @yaml
+      (0 .. hierarchy.size - 2).each do |depth|
+        focus = focus.fetch(hierarchy[depth])
+      end
+      focus[hierarchy[-1]] = Mustache.render(value, @secrets) #replace with  value of mustache placeholder
+
+    end
 
     def manifest_yaml
       YAML.dump(@yaml, options = {:line_width => -1})
