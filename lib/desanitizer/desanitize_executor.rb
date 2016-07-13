4e5cf72a6dfe8e56405a0cda352d31b06c565812
Topher Lubaway <clubaway@pivotal.io>
2016-07-13 11:56:15 -0700

    Adds error handling for parsing a file with secrets
    if that file has no corresponding secret file
    
    [#126178599]
    
    Signed-off-by: Nolan Varani <nvarani@pivotal.io>

--- lib/desanitizer/desanitize_executor.rb
+++ lib/desanitizer/desanitize_executor.rb
@@ -5,7 +5,7 @@ module Desanitizer
   class DesanitizeExecutor
     def self.execute(manifest_path, secrets_path, logger = Logger.new(STDERR))
 
-      manifest = YAML.load_file(manifest_path)
+      @manifest = YAML.load_file(manifest_path)
 
       # if given a secrets directory, choose appropriate secrets file
       if File.directory?(secrets_path)
@@ -18,11 +18,17 @@ module Desanitizer
       if File.exist?(secrets_path)
         secrets = JSON.parse(File.read(secrets_path))
       else
-        logger.warn "Secrets file not present for YAML file #{manifest_path} skipping it"
-        return
+        unless unresolved_secrets?
+          logger.warn "Secrets file not present for YAML file #{manifest_path} skipping it"
+          return
+        else
+          logger.error "\e[31m This manifest #{manifest_path} has secrets but no corresponding secrets file \e[0m "
+          exit 1
+        end
       end
-      interpolator = Desanitizer::MustacheInterpolator.new(manifest, secrets, logger)
-      Sanitizer::YamlTraverser.traverse(manifest) do |k, v, h|
+
+      interpolator = Desanitizer::MustacheInterpolator.new(@manifest, secrets, logger)
+      Sanitizer::YamlTraverser.traverse(@manifest) do |k, v, h|
         interpolator.interpolate(k, v, h)
       end
 
@@ -30,5 +36,10 @@ module Desanitizer
         file.write interpolator.manifest_yaml
       end
     end
+
+    def self.unresolved_secrets?
+      @manifest.to_s.match(/"{{.*}}"/)
+    end
+
   end
 end
