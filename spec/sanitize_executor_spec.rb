c3954b1afde549133ce20da186af86f695ad3ec0
Derek Reeve <dreeve@pivotal.io>
2016-08-26 11:14:30 -0700

    Refactor sanitize tests
    remove unneded config files
    
    move tests to standard from desanitize
    
    [#128624669] Create a script to desanitize, sanitize, and generate manifest
    
    Signed-off-by: Topher Lubaway <clubaway@pivotal.io>

--- spec/sanitize_executor_spec.rb
+++ spec/sanitize_executor_spec.rb
@@ -29,10 +29,13 @@
 require 'spec_helper'
 
 describe Sanitizer::SanitizeExecutor do
+  let(:work_dir) { File.dirname(__FILE__) }
+  let(:default_config_path) { File.join(spec_folder_root, "../config/catchall") }
+  let(:spec_folder_root) { File.expand_path(File.dirname(__FILE__)) }
+
   before :each do
-    @work_dir = File.dirname(__FILE__)
     @tmp_dir=`mktemp -d`.strip
-    `cp -rf #{@work_dir}/fixture/* #{@tmp_dir}/`
+    `cp -rf #{work_dir}/fixture/* #{@tmp_dir}/`
   end
 
   after :each do
@@ -56,66 +59,49 @@ describe Sanitizer::SanitizeExecutor do
   end
 
 
-  it 'extracts secrets to another file and replaces with mustache style syntax' do
-    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_1.yml",  "#{@tmp_dir}/config_1", "#{@tmp_dir}")
-    keys=['bla','foo', 'bar_secret_key']
-
-    manifest_post_sanitize = check_sanitize_success('manifest_1',keys, 'bar_secret_value')
-
-    not_secret_node = manifest_post_sanitize['bla']['foo']['bar_not_secret_key']
-    expect(not_secret_node).to eq("bar_not_secret_value")
-    special_char_value_node = manifest_post_sanitize['bla']['foo']['special_char_value_key']
-    expect(special_char_value_node).to eq("*")
+  it 'extracts secrets to another file' do
+    FileUtils.rm("#{@tmp_dir}/secrets-manifest_1.json")
+    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_1.yml", default_config_path, "#{@tmp_dir}")
+    expect { File.open("#{@tmp_dir}/secrets-manifest_1.json") }.to_not raise_error
   end
 
-  it 'works with multiple match' do
-    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_1.yml",  "#{@tmp_dir}/config_2", "#{@tmp_dir}")
-    keys=['bla','foo', 'bar_secret_key']
-    check_sanitize_success('manifest_1',keys, 'bar_secret_value')
-    keys=['bla','foo', 'bar_secret_key_1']
-    manifest_post_sanitize = check_sanitize_success('manifest_1',keys, 'bar_secret_value_1')
-    special_char_value_node = manifest_post_sanitize['bla']['foo']['special_char_value_key']
-    expect(special_char_value_node).to eq("*")
+  it 'adds \'"{{\' to the sanitized file' do
+    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_1.yml", default_config_path, "#{@tmp_dir}")
+    expect(File.open("#{@tmp_dir}/manifest_1.yml").grep(/\"\{\{/)).to_not be_empty
   end
 
   it 'works with multiple line value keys' do
-    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_3.yml",  "#{@tmp_dir}/config_3", "#{@tmp_dir}")
-    expect_value = \
-"----------BEGIN RSA PRIVATE KEY--------
-ASDASDASDSADSAD
-----------END RSA PRIVATE KEY----------
-"
-    keys=['bla','foo', 'multi_line_value_key']
-    check_sanitize_success('manifest_3',keys, expect_value)
+    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_3.yml",  default_config_path, "#{@tmp_dir}")
+    expect(compare_yml("#{@tmp_dir}/manifest_3.yml", "#{work_dir}/fixture/sanitized_manifest_3.yml")).to be_truthy
   end
 
   it 'works with multiple line value keys' do
-    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_4.yml",  "#{@tmp_dir}/config_4", "#{@tmp_dir}")
+    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_4.yml",  default_config_path, "#{@tmp_dir}")
     keys=['instance_groups', 0 , 'templates', 0, 'properties', 'tsa', 'private_key']
     check_sanitize_success('manifest_4', keys, 'redacted')
   end
 
   it 'does not create secret file if no secrets matched the pattern' do
-    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_1.yml",  "#{@tmp_dir}/config_4", "#{@tmp_dir}")
-    expect(File).to_not exist("#{@tmp_dir}/secrets-manifest_1.json")
+    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/non_matching_secret_manifest.yml",  default_config_path, "#{@tmp_dir}")
+    expect(File).to_not exist("#{@tmp_dir}/secrets-non_matching_secret_manifest.json")
   end
 
   it 'ignores values already in mustache syntax' do
-    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_5.yml",  "#{@tmp_dir}/config_1", "#{@tmp_dir}", Logger.new(nil))
+    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_5.yml",  default_config_path, "#{@tmp_dir}", Logger.new(nil))
     manifest_post_sanitize = YAML.load_file("#{@tmp_dir}/manifest_5.yml")
     mustache_value_key = manifest_post_sanitize['bla']['foo']['bar_secret_key']
     expect(mustache_value_key).to eq("{{bar_secret_value}}")
   end
 
   it 'ignores null or empty values for matching keys' do
-    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_6.yml",  "#{@tmp_dir}/config_1", "#{@tmp_dir}", Logger.new(nil))
+    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_6.yml",  default_config_path, "#{@tmp_dir}", Logger.new(nil))
     manifest_post_sanitize = YAML.load_file("#{@tmp_dir}/manifest_6.yml")
     mustache_value_key = manifest_post_sanitize['bla']['foo']['bar_secret_key']
     expect(mustache_value_key).to eq("{{bla_foo_bar_secret_key}}")
   end
 
   it 'ignores values already in spiff syntax' do
-    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_7.yml",  "#{@tmp_dir}/config_1", "#{@tmp_dir}", Logger.new(nil))
+    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_7.yml",  default_config_path, "#{@tmp_dir}", Logger.new(nil))
     manifest_post_sanitize = YAML.load_file("#{@tmp_dir}/manifest_7.yml")
     mustache_value_key = manifest_post_sanitize['bla']['foo']['bar_secret_key']
     expect(mustache_value_key).to eq("((bar_secret_value))")
@@ -131,7 +117,7 @@ ASDASDASDSADSAD
       file.write existing_secrets.to_json
     end
 
-    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_1.yml",  "#{@tmp_dir}/config_1", "#{@tmp_dir}")
+    Sanitizer::SanitizeExecutor.execute("#{@tmp_dir}/manifest_1.yml",  default_config_path, "#{@tmp_dir}")
     keys=['bla','foo', 'bar_secret_key']
 
     manifest_post_sanitize = check_sanitize_success('manifest_1',keys, 'bar_secret_value')
