da03752e49b9ddc2c94fce0a374964d4e548d902
Derek Reeve <dreeve@pivotal.io>
2016-08-26 11:56:18 -0700

    Error handling spec cleanup.
    * Use ruby standard library instead of shelling out
    * Use let statements instead of instance variables
    * Use real fake dirs. Come on down!
    
    [#128624669] Create a script to desanitize, sanitize, and generate manifest
    
    Signed-off-by: Topher Lubaway <clubaway@pivotal.io>

--- spec/error_handling_spec.rb
+++ spec/error_handling_spec.rb
@@ -30,22 +30,22 @@ require 'spec_helper'
 
 
 describe "error handling" do
+	let(:work_dir) { File.dirname(__FILE__) }
+  let(:tmp_dir) { Dir.mktmpdir }
+  let(:desanitizer_executable) { "#{work_dir}/../bin/desanitize" }
+  let(:fake_dir) { Dir.mktmpdir }
 
-	before :each do
-		@work_dir = File.dirname(__FILE__)
-		@tmp_dir=`mktemp -d`.strip
-		@fake_dir=`mktemp -d`.strip
-		`cp -rf #{@work_dir}/fixture/* #{@tmp_dir}/`
-	end
+  before do
+    FileUtils.cp_r Dir.glob("#{work_dir}/fixture/*"), tmp_dir
+  end
 
-	after :each do
-		`rm -rf #{@tmp_dir}`
-		`rm -rf #{@fake_dir}`
-	end
-
+  after do
+    FileUtils.rm_r tmp_dir
+    FileUtils.rm_r fake_dir
+  end
 
 	it 'Throws an error when secrets exist in a manifest, but there is no corresponding secrets file' do
-		expect(system("#{@work_dir}/../bin/desanitize -s #{@fake_dir} -i #{@tmp_dir}/manifest_5.yml 2>/dev/null")).to eq(false)
+		expect(system("#{desanitizer_executable} -s #{fake_dir} -i #{tmp_dir}/manifest_5.yml 2>/dev/null")).to be_falsy
 	end
 
 end
